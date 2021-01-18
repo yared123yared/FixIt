c9b170f16632daa68ca8ecca20e98e67bc5c67a2
fasikawkn <fasikawkn@gmail.com>
2021-01-18 03:59:39 +0300

    ui commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 
-import 'Screens/Category_main_screen.dart';
-import 'Screens/Customer_main_screen.dart';
+import 'Screens/customer_home.dart';
 
-
-
-void main() => runApp(MyApp());
+void main(){
+  runApp(MyApp());
+}
 
 class MyApp extends StatefulWidget {
   @override
@@ -13,73 +12,31 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
-
-
-
-
-
-
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DeliMeals',
       theme: ThemeData(
-          primarySwatch: Colors.pink,
-          accentColor: Colors.amber,
-          canvasColor: Color.fromRGBO(225, 254, 229, 1),
-          fontFamily: 'Raleway',
-          textTheme: ThemeData.light().textTheme.copyWith(
-              bodyText1: TextStyle(
-                color: Color.fromRGBO(20, 51, 51, 1),
-              ),
-              bodyText2: TextStyle(
-                color: Color.fromRGBO(20, 31, 51, 1),
-              ),
-              headline6: TextStyle(
-                  fontSize: 24,
-                  fontFamily: 'RobotoCondensed'
-              )
+        primarySwatch: Colors.purple,
+        textTheme: ThemeData.light().textTheme.copyWith(
+          subtitle1: TextStyle(
+            color: Colors.purple,
+          ),
+          bodyText1: TextStyle(
+            color:Colors.grey
+          ),
+            subtitle2: TextStyle(
+              color: Colors.pink,
+            ),
+            bodyText2: TextStyle(
 
-          )
+            )
+        )
       ),
 
-      initialRoute:'/',
+      initialRoute: '/',
       routes: {
-        '/' : (ctx)=>CategoryMainScreen(),
-        '/customer' : (ctx)=>CustomerMainScreen(),
-        '/customerr' : (ctx)=>CustomerMainScreen(),
-        '/customerrd' : (ctx)=>CustomerMainScreen(),
-
-
-
-
-      },
-      onUnknownRoute: (settings){
-        return MaterialPageRoute(builder: (ctx)=>CategoryMainScreen());
+        '/': (context) => CustomerHome(),
       },
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('DeliMeals'),
-      ),
-      body: Center(
-        child: Text('Navigation Time!'),
-      ),
-    );
-
   }
 }
